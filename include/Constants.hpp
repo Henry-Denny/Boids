266f71604144490c3ea195612606dd7d83cca77e
Henry Denny1 <henry.denny12@gmail.com>
2021-11-17 23:10:56 +0000

    Update some constants

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -8,8 +8,8 @@ namespace constants
     const double k_alignmentCoeff { 0.5f };
     const double k_cohesionCoeff { 10.0f };
     const double k_avoidanceCoeff { 2000.0f };
-    const double k_orbitalCoeff { 1.0f };
-    const double k_resistanceCoeff { 1.0f };
+    const double k_orbitalCoeff { 20.0f };
+    const double k_resistanceCoeff { 20.0f };
     const double k_visionRadius { 40.0f };
     const double k_initialSpeed { 40.0f };
     const double k_maxForce { 2000.0f };
